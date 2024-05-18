6b411b240e2b7f9340cc3fec56087c6b5ef1e0f2
fahiid33 <trey34809@gmail.com>
2024-05-18 20:50:05 +0100

    deployment almost done

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,15 +1,17 @@
 import CategoryList from "@/components/category";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 async function getAllListsByCategory(getId: string) {
   try {
-    const res = await fetch(`${process.env.URL}/api/category?categoryID=${getId}`, {
-      method: "GET",
-      cache: "no-store",
+    const lists = await prisma.post.findMany({
+      where: {
+        category: getId,
+      },
     });
-
-    const data = await res.json();
-
-    if (data.success) return data.data;
+    console.log("Fetched lists:", lists); // Log fetched lists
+    return lists;
   }
   catch (e) {
     console.log(e);
@@ -19,7 +21,7 @@ async function getAllListsByCategory(getId: string) {
 export default async function Category({ params }: { params: any }) {
   const { id } = params;
 
-  const getAllList = await getAllListsByCategory(id);
+  const getAllList = await getAllListsByCategory(id) as any;
 
   return <CategoryList list={getAllList} />;
 }
