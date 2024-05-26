57910e6ba51a02bab5095790dea049d5f5ec5243
fahiid33 <trey34809@gmail.com>
2024-05-26 06:03:08 +0100

    expresso

--- src/app/api/blog-post/get-all-posts/route.ts
+++ src/app/api/blog-post/get-all-posts/route.ts
@@ -2,8 +2,17 @@ import prisma from "@/database";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+
+  // const {page, limit} = request.query;
+
+  // const currentPage = page ? Number(page) : 1;
+  // const currentLimit = limit ? Number(limit) : 10;
+
   try {
-    const getAllBlogPosts = await prisma.post.findMany();
+
+    const getAllBlogPosts = await prisma.post.findMany(
+
+    );
     if (getAllBlogPosts && getAllBlogPosts.length) {
       return NextResponse.json({
         success: true,
